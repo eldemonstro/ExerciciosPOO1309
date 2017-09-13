6a890698fe96c70e1a4d75fb4cfca172081efd0e
Fabricio Bezerra <demonstro003@gmail.com>
2017-09-13 09:46:31 -0300

    exercicio 29

---
+++ src/exercicio29/Main.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio29;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+* 29. Faça um algoritmo que receba o número do
+* mês e mostre o mês correspondente. Valide
+* mês inválido.
+*/
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Entre com um numero para ser retornado um mês: ");        
+        try{
+            int i = Integer.parseInt(br.readLine());
+            String mes;
+            switch (i) {
+                case 1:
+                    mes = "Janeiro";
+                    break;
+                case 2:
+                    mes = "Fevereiro";
+                    break;
+                case 3:
+                    mes = "Março";
+                    break;
+                case 4:
+                    mes = "Abril";
+                    break;
+                case 5:
+                    mes = "Maio";
+                    break;
+                case 6:
+                    mes = "Junho";
+                    break;
+                case 7:
+                    mes = "Julho";
+                    break;
+                case 8:
+                    mes = "Agosto";
+                    break;
+                case 9:
+                    mes = "Setembro";
+                    break;
+                case 10:
+                    mes = "Outubro";
+                    break;
+                case 11:
+                    mes = "Novembro";
+                    break;
+                case 12:
+                    mes = "Dezembro";
+                    break;
+                default:
+                    System.err.println("Numero não valido");
+                    return;
+            }
+            System.out.println("O mês correspondente ao numero " + i + " é " + mes);
+        }catch(NumberFormatException nfe){
+            System.err.println("Numero não valido");
+        }
+    }
+    
+}
